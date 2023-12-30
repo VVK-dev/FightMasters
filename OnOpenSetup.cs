d6dd3e0534588df5372879dbe6bac0ec3776ea36
VVk-dev <varunvenkwrk@gmail.com>
2023-12-29 21:22:20 -0500

    Add method to check for existence of necessary files, and added method signature of method to check for corrupted files.

--- OnOpenSetup.cs
+++ OnOpenSetup.cs
@@ -9,12 +9,62 @@ namespace FightMasters
     public abstract class OnOpenSetup
     {
 
-        public void Setup()
+        public static ICard[] BaseDeck { get; private set; }//In order to properly check for corruption, all
+                                                            //neutral card objects are created once and
+                                                            //stored in this array.
+
+        public static void Setup()
         {
 
-            //TO DO: UNIT TESTING
+            //Checking if necessary files exist:
+
+            //U stands for unknown, M for missing, C for corrupt and G for good
+
+            Dictionary<string, string> FileCondition = new() { 
+                
+                { "ICard.cs", "U"}, { "ICharClass.cs", "U" }, { "IToken.cs", "U"}, { "IMinion.cs", "U"}, 
+                { "Damage.cs", "U"}, { "CharacterClasses.cs", "U"}, { "Player.cs", "U"}, 
+                { "PlayHelper.cs", "U"}, { "TokenHandler.cs", "U"}, { "ClassCards.cs", "U"}, 
+                { "NeutralCards.cs", "U"}, { "ClassMinions.cs", "U"}, { "NeutralMinions.cs", "U"}, 
+                { "MainMenu.cs", "U"}
+
+            };
+
+            FileCondition = FileCorruptionCheck(FileExistenceCheck(FileCondition));
 
 
+
+
+        }
+
+        private static Dictionary<string,string> FileExistenceCheck(Dictionary<string,string> FileCondition) {
+
+            foreach (KeyValuePair<string,string> file in FileCondition)
+            {
+
+                if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), file.Key)))
+                {
+                    FileCondition[file.Key] = "M";
+                }
+
+            }
+
+            return FileCondition;
+
+        }
+
+        private static Dictionary<string, string> FileCorruptionCheck(Dictionary<string, string> FileCondition)
+        {
+
+            //TO DO: CHECK FOR CORRUPTED FILES
+
+            
+
+
+
+
+
+            return FileCondition;
 
         }
 

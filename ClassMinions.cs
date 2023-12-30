8ca2291b0f575dc25d30bfec66b826b5271ef70a
VVk-dev <varunvenkwrk@gmail.com>
2023-12-29 21:26:09 -0500

    Created a class to hold class specific minions

---
+++ ClassMinions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightMasters
+{
+    public class ClassMinions
+    {
+
+        //STORAGE FOR ALL CLASS SPECIFIC MINIONS
+
+
+
+
+
+
+
+
+
+    }
+}

ed8aec1ec3ad4e8b746d6868a5b4e003b5243377
VVk-dev <varunvenkwrk@gmail.com>
2024-01-05 16:22:40 -0500

    Created new Interface as many things are common between ICard and IMinion

---
+++ IPlayable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightMasters
+{
+    public interface IPlayable
+    {
+
+        string Name { get; }
+
+        Damage[]? DamageDealt { get; } //List of damage done by this Item, as a single playable item (either
+                                       //minion or card) can do multiple types of damage with varying values.
+
+        int Heal { get; } //How much the playable item heals a player for
+
+        Dictionary<string, List<IToken>>? TokensAppliedCaster { get; }
+        //Key = type of token applied as string, Value = tokens applied 
+        Dictionary<string, List<IToken>>? TokensAppliedOpponent { get; }
+        //Key = type of token applied as string, Value = tokens applied 
+
+    }
+}
